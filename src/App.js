d8f6050af8f3a1d1c9ffec723c45567aa02992fd
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2024-03-19 10:23:20 +0330

    Adding and listing tasks from state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Task from './Task.js';
 import TaskForm from './TaskForm.js'
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  function addTask(name){
+    setTasks(prev => {
+      return [...prev, {name:name, done:false}]
+    })
+  }
   return (
    <main>
-    <TaskForm />
-    <Task />
-    <Task />
-    <Task />
-    <Task />
-    <Task />
-    <Task />
+    <TaskForm onAdd={addTask} />
+    {tasks.map(task=>(
+      <Task {...task} />
+    ))}
+ 
    </main>
   );
 }
