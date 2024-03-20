64afd89f244f47272e0ae27f57d38756e4c3f3aa
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2024-03-20 16:45:26 +0330

    Renaming tasks

--- src/App.js
+++ src/App.js
@@ -1,70 +1,79 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Task from './Task.js';
-import TaskForm from './TaskForm.js'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Task from "./Task.js";
+import TaskForm from "./TaskForm.js";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  useEffect(()=>{
-    if(tasks.length === 0) return;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks])
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
-  useEffect(()=>{  
-    const tasks = JSON.parse(localStorage.getItem('tasks'));
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
     setTasks(tasks || []);
-  }, [])
+  }, []);
 
-  
-  function addTask(name){
-    setTasks(prev => {
-      return [...prev, {name:name, done:false}]
-    })
+  function addTask(name) {
+    setTasks((prev) => {
+      return [...prev, { name: name, done: false }];
+    });
   }
 
-
-  function removeTask(indexToRemove){
-    setTasks(prev => {
-      return prev.filter((taskObject, index) => index !== indexToRemove )
-    })
+  function removeTask(indexToRemove) {
+    setTasks((prev) => {
+      return prev.filter((taskObject, index) => index !== indexToRemove);
+    });
   }
 
-  function updateTaskDone(taskIndex, newDone){
-    setTasks(prev => {
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks((prev) => {
       const newTasks = [...prev];
       newTasks[taskIndex].done = newDone;
       return newTasks;
-    })
+    });
   }
 
-  const numberComplete = tasks.filter(t => t.done).length;
+  const numberComplete = tasks.filter((t) => t.done).length;
   const numberTotal = tasks.length;
 
+  function getMessage() {
+    const percentage = (numberComplete / numberTotal) * 100;
+    if (percentage === 0) {
+      return "سعی کن حداقل یکیشو انجام بدی:) 🙏";
+    }
+    if (percentage === 100) {
+      return "درود. تمامی کار ها انجام شد. 😉🤙";
+    }
+    return "آفرین با قدرت ادامه بده💪";
+  }
 
-  function getMessage(){
-    const percentage = numberComplete/numberTotal * 100;
-    if(percentage === 0){
-      return 'سعی کن حداقل یکیشو انجام بدی:) 🙏'
-    }
-    if(percentage === 100){
-      return 'درود. تمامی کار ها انجام شد. 😉🤙'
-    }
-    return 'آفرین با قدرت ادامه بده💪'
+  function renameTask(index, newName) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    });
   }
 
   return (
-   <main>
-    <h1>{numberComplete}/{numberTotal} complete</h1>
-    <h2>{getMessage()}</h2>    
-    <TaskForm onAdd={addTask} />
-    {tasks.map((task, index)=>(
-      <Task {...task}
-      onTrash={()=> removeTask(index)} 
-      onToggle={done => updateTaskDone(index, done) } />
-    ))}
- 
-   </main>
+    <main>
+      <h1>
+        {numberComplete}/{numberTotal} مورد انجام شده
+      </h1>
+      <h2>{getMessage()}</h2>
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task, index) => (
+        <Task
+          {...task}
+          onRename={newName => renameTask(index,newName)}
+          onTrash={() => removeTask(index)}
+          onToggle={(done) => updateTaskDone(index, done)}
+        />
+      ))}
+    </main>
   );
 }
 
